dde78482cadad3f86957f81f12f4aef07adfe3eb
Pieter Pas <pieter.pas@outlook.com>
2023-08-22 17:43:05 +0200

    [Py] improve QueryPythonForPybind11.cmake

--- interfaces/python/cmake/QueryPythonForPybind11.cmake
+++ interfaces/python/cmake/QueryPythonForPybind11.cmake
@@ -6,9 +6,73 @@ mark_as_advanced(USE_GLOBAL_PYBIND11)
 # find_package(pybind11 REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH).
 function(find_pybind11_python_first)
 
-    find_package(Python3 REQUIRED COMPONENTS Interpreter Development)
-    if (NOT USE_GLOBAL_PYBIND11)
-        # Query Python to see if it knows where the headers are
+    # Find Python
+    if (CMAKE_CROSSCOMPILING AND FALSE)
+        find_package(Python3 REQUIRED COMPONENTS Development.Module)
+    else()
+        find_package(Python3 REQUIRED COMPONENTS Interpreter Development.Module)
+    endif()
+
+    # Tweak extension suffix and debug ABI when cross-compiling
+    if (CMAKE_CROSSCOMPILING)
+        if (NOT PY_BUILD_EXT_SUFFIX AND DEFINED TOOLCHAIN_Python3_EXT_SUFFIX)
+            set(PY_BUILD_EXT_SUFFIX ${TOOLCHAIN_Python3_EXT_SUFFIX})
+        endif()
+        # SETUPTOOLS_EXT_SUFFIX environment variable
+        if (NOT PY_BUILD_EXT_SUFFIX AND DEFINED ENV{SETUPTOOLS_EXT_SUFFIX})
+            message(STATUS "Setting PY_BUILD_EXT_SUFFIX to "
+                "ENV{SETUPTOOLS_EXT_SUFFIX}: $ENV{SETUPTOOLS_EXT_SUFFIX}")
+            set(PY_BUILD_EXT_SUFFIX $ENV{SETUPTOOLS_EXT_SUFFIX})
+        endif()
+        # If that still didn't work, use the Python3_SOABI variable:
+        if (NOT PY_BUILD_EXT_SUFFIX AND Python3_SOABI)
+            message(STATUS "Determining Python extension suffix based on "
+                    "Python3_SOABI.")
+            if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+                set(PY_BUILD_EXTENSION ".pyd")
+            else()
+                set(PY_BUILD_EXTENSION "${CMAKE_SHARED_MODULE_SUFFIX}")
+            endif()
+            set(PY_BUILD_EXT_SUFFIX ".${Python3_SOABI}${PY_BUILD_EXTENSION}")
+        endif()
+        # Sanity checks:
+        if (NOT PY_BUILD_EXT_SUFFIX)
+            message(FATAL_ERROR "Unable to determine extension suffix.\
+                Try manually setting PY_BUILD_EXT_SUFFIX.")
+        endif()
+        if (Python3_SOABI AND
+                NOT PY_BUILD_EXT_SUFFIX MATCHES "\\.${Python3_SOABI}\\.")
+            message(WARNING "PY_BUILD_EXT_SUFFIX (${PY_BUILD_EXT_SUFFIX}) "
+                "does not match Python3_SOABI (${Python3_SOABI})")
+        endif()
+        # Check the debug ABI:
+        if (NOT PY_BUILD_DEBUG_ABI AND DEFINED TOOLCHAIN_Python3_DEBUG_ABI)
+            set(PY_BUILD_DEBUG_ABI ${TOOLCHAIN_Python3_DEBUG_ABI})
+        endif()
+        # Otherwise, try to deduce it from the SOABI:
+        if (NOT DEFINED PY_BUILD_DEBUG_ABI)
+            if (PY_BUILD_EXT_SUFFIX MATCHES "[0-9]+d-")
+                set(PY_BUILD_DEBUG_ABI true)
+            else()
+                set(PY_BUILD_DEBUG_ABI false)
+            endif()
+        endif()
+        # Cache the result:
+        set(PY_BUILD_EXT_SUFFIX ${PY_BUILD_EXT_SUFFIX} CACHE STRING
+            "The extension for Python extension modules")
+        set(PY_BUILD_DEBUG_ABI ${PY_BUILD_DEBUG_ABI} CACHE BOOL
+            "Whether to compile for a debug version of Python")
+        # Override pybind11NewTools.cmake's PYTHON_MODULE_EXTENSION variable:
+        message(STATUS "Python extension suffix: ${PY_BUILD_EXT_SUFFIX}")
+        message(STATUS "Python debug ABI: ${PY_BUILD_DEBUG_ABI}")
+        set(PYTHON_MODULE_EXTENSION ${PY_BUILD_EXT_SUFFIX}
+            CACHE INTERNAL "" FORCE)
+        set(PYTHON_IS_DEBUG ${PY_BUILD_DEBUG_ABI}
+            CACHE INTERNAL "" FORCE)
+    endif()
+
+    # Query Python to see if it knows where the pybind11 root is
+    if (NOT USE_GLOBAL_PYBIND11 AND Python3_EXECUTABLE)
         if (NOT pybind11_ROOT OR NOT EXISTS ${pybind11_ROOT})
             message(STATUS "Detecting pybind11 CMake location")
             execute_process(COMMAND ${Python3_EXECUTABLE}
@@ -29,49 +93,5 @@ function(find_pybind11_python_first)
 
     # pybind11 is header-only, so finding a native version is fine
     find_package(pybind11 ${ARGN} REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH)
-
-    # Tweak extension suffix when cross-compiling
-    if (CMAKE_CROSSCOMPILING)
-        if (NOT PY_BUILD_EXT_SUFFIX AND DEFINED ENV{SETUPTOOLS_EXT_SUFFIX})
-            set(PY_BUILD_EXT_SUFFIX $ENV{SETUPTOOLS_EXT_SUFFIX})
-        endif()
-        if (NOT PY_BUILD_EXT_SUFFIX)
-            message(STATUS "Determining Python extension suffix")
-            # Find the python3.x-config script in the sysroot instead of on the
-            # build system:
-            find_program(PY_BUILD_Python3_CONFIG
-                python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}-config
-                ONLY_CMAKE_FIND_ROOT_PATH)
-            # Report errors:
-            if (NOT PY_BUILD_Python3_CONFIG)
-                message(FATAL_ERROR "Unable to find python3-config."
-                    "\nTry manually setting PY_BUILD_EXT_SUFFIX.")
-            else()
-                # If we found the python3.x-config script, query it for the
-                # extension suffix:
-                execute_process(COMMAND ${PY_BUILD_Python3_CONFIG}
-                    --extension-suffix
-                    OUTPUT_VARIABLE PY_BUILD_EXT_SUFFIX
-                    ERROR_VARIABLE PY_BUILD_EXT_SUFFIX_ERR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE
-                    RESULT_VARIABLE PY_BUILD_EXT_SUFFIX_RESULT)
-                # Report errors:
-                if (NOT PY_BUILD_EXT_SUFFIX_RESULT EQUAL 0
-                    OR NOT PY_BUILD_EXT_SUFFIX)
-                    message(FATAL_ERROR "Unable to determine extension suffix:"
-                        "\n${PY_BUILD_EXT_SUFFIX}"
-                        "\n${PY_BUILD_EXT_SUFFIX_ERR}"
-                        "\nTry manually setting PY_BUILD_EXT_SUFFIX.")
-                endif()
-                # Cache the result:
-                set(PY_BUILD_EXT_SUFFIX ${PY_BUILD_EXT_SUFFIX} CACHE STRING
-                    "The extension for Python extension modules")
-            endif()
-        endif()
-        # Override pybind11NewTools.cmake's PYTHON_MODULE_EXTENSION variable:
-        message(STATUS "Python extension suffix: ${PY_BUILD_EXT_SUFFIX}")
-        set(PYTHON_MODULE_EXTENSION ${PY_BUILD_EXT_SUFFIX}
-            CACHE INTERNAL "" FORCE)
-    endif()
 
 endfunction()
