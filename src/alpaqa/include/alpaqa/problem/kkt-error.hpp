0aad8250deba3f92c59d69f80a1352d45f5d33b2
Pieter Pas <pieter.pas@outlook.com>
2023-08-28 21:33:03 +0200

    Add bounds_violation to KKTError

--- src/alpaqa/include/alpaqa/problem/kkt-error.hpp
+++ src/alpaqa/include/alpaqa/problem/kkt-error.hpp
@@ -9,14 +9,16 @@ template <Config Conf>
 struct KKTError {
     USING_ALPAQA_CONFIG(Conf);
     static constexpr auto NaN = alpaqa::NaN<config_t>;
-    real_t stationarity = NaN, constr_violation = NaN, complementarity = NaN;
+    real_t stationarity = NaN, constr_violation = NaN, complementarity = NaN,
+           bounds_violation = NaN;
 };
 
 template <Config Conf>
-KKTError<Conf> compute_kkt_error(const alpaqa::TypeErasedProblem<Conf> &problem,
-                                 alpaqa::crvec<Conf> x, alpaqa::crvec<Conf> y) {
+KKTError<Conf> compute_kkt_error(const TypeErasedProblem<Conf> &problem, crvec<Conf> x,
+                                 crvec<Conf> y) {
 
     USING_ALPAQA_CONFIG(Conf);
+    using vec_util::norm_inf;
     const auto n = x.size(), m = y.size();
     vec z(n), grad_Lx(n), work(n), g(m), e(m);
     // Gradient of Lagrangian, ∇ℒ(x,y) = ∇f(x) + ∇g(x) y
@@ -24,20 +26,25 @@ KKTError<Conf> compute_kkt_error(const alpaqa::TypeErasedProblem<Conf> &problem,
     // Eliminate normal cone of bound constraints, z = Π(x - ∇ℒ(x,y)) - x
     problem.eval_prox_grad_step(1, x, grad_Lx, work, z);
     // Stationarity, ‖Π(x - ∇ℒ(x,y)) - x‖
-    auto stationarity = alpaqa::vec_util::norm_inf(z);
+    auto stationarity = norm_inf(z);
     // Constraints, g(x)
     problem.eval_g(x, g);
     // Distance to feasible set, e = g(x) - Π(g(x))
     problem.eval_proj_diff_g(g, e);
     // Constraint violation, ‖g(x) - Π(g(x))‖
-    auto constr_violation = alpaqa::vec_util::norm_inf(e);
+    auto constr_violation = norm_inf(e);
     // Complementary slackness
     real_t complementarity = std::inner_product(
         y.begin(), y.end(), e.begin(), real_t(0),
         [](real_t acc, real_t ye) { return std::fmax(acc, std::abs(ye)); }, std::multiplies<>{});
+    // Bounds violation
+    real_t bounds_violation = NaN<config_t>;
+    if (problem.provides_get_box_C())
+        bounds_violation = norm_inf(project(x, problem.get_box_C()) - x);
     return {.stationarity     = stationarity,
             .constr_violation = constr_violation,
-            .complementarity  = complementarity};
+            .complementarity  = complementarity,
+            .bounds_violation = bounds_violation};
 }
 
 } // namespace alpaqa
