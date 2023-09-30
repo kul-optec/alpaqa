5227bec576ded185d5c0b3ab856f8c15e9c2aa6f
Pieter Pas <pieter.pas@outlook.com>
2023-10-01 00:10:00 +0200

    Use mo_seq_cst for AtomicStopSignal::stop()

--- src/alpaqa/include/alpaqa/util/atomic-stop-signal.hpp
+++ src/alpaqa/include/alpaqa/util/atomic-stop-signal.hpp
@@ -9,11 +9,11 @@ class AtomicStopSignal {
     AtomicStopSignal() = default;
     AtomicStopSignal(const AtomicStopSignal &) : AtomicStopSignal() {}
     AtomicStopSignal &operator=(const AtomicStopSignal &) = delete;
-    AtomicStopSignal(AtomicStopSignal &&) : AtomicStopSignal() {}
-    AtomicStopSignal &operator=(AtomicStopSignal &&) { return *this; }
+    AtomicStopSignal(AtomicStopSignal &&) noexcept : AtomicStopSignal() {}
+    AtomicStopSignal &operator=(AtomicStopSignal &&) noexcept { return *this; }
 
-    void stop() { stop_flag.store(true, std::memory_order_relaxed); }
-    bool stop_requested() const {
+    void stop() { stop_flag.store(true, std::memory_order_seq_cst); }
+    [[nodiscard]] bool stop_requested() const {
         return stop_flag.load(std::memory_order_relaxed);
     }
 
