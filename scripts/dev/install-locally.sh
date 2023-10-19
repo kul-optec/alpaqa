8da4dc47adae691e8c92b1ec603a73e79787ed0f
Pieter Pas <pieter.pas@outlook.com>
2023-10-19 16:01:31 +0200

    Update install-locally.sh

--- scripts/dev/install-locally.sh
+++ scripts/dev/install-locally.sh
@@ -16,6 +16,14 @@ if [ ! -d "$pfx" ]; then
         tar xJ -C "$tools_dir"
 fi
 
+# Install QPALM
+if [ ! -d "$pfx/qpalm" ]; then
+    qpalm_download="https://github.com/kul-optec/QPALM/releases/download/1.2.1"
+    wget "$qpalm_download/QPALM-1.2.1-Linux.tar.gz" -O- | \
+        tar xz -C "$pfx"
+    mv "$pfx/QPALM-1.2.1-Linux" "$pfx/qpalm"
+fi
+
 # Use ccache to cache compilation
 export CMAKE_C_COMPILER_LAUNCHER=ccache
 export CMAKE_CXX_COMPILER_LAUNCHER=ccache
@@ -28,12 +36,15 @@ export FCFLAGS="-march=native -fdiagnostics-color"
 cmake -S. -Bbuild-local \
     --toolchain "$pfx/$triple.toolchain.cmake" \
     -DCMAKE_PREFIX_PATH="$pfx/mumps/usr/local;$pfx/ipopt/usr/local" \
-    -DCMAKE_FIND_ROOT_PATH="$pfx/eigen;$pfx/casadi;$pfx/openblas;$pfx/mumps;$pfx/ipopt" \
+    -DCMAKE_FIND_ROOT_PATH="$pfx/eigen;$pfx/casadi;$pfx/openblas;$pfx/mumps;$pfx/ipopt;$pfx/qpalm" \
     -DCMAKE_POSITION_INDEPENDENT_CODE=On \
     -DBUILD_SHARED_LIBS=On \
     -DALPAQA_WITH_DRIVERS=On \
     -DALPAQA_WITH_EXAMPLES=Off \
     -DALPAQA_WITH_TESTS=Off \
+    -DALPAQA_WITH_CUTEST=On \
+    -DALPAQA_WITH_QPALM=On \
+    -DALPAQA_WITH_IPOPT=On \
     -DALPAQA_WITH_PYTHON=Off \
     -DCMAKE_STAGING_PREFIX="$PWD/staging" \
     -G "Ninja Multi-Config"
@@ -59,10 +70,11 @@ CMAKE_FIND_ROOT_PATH = "$pfx/pybind11-master;$pfx/casadi;$pfx/eigen-master"
 USE_GLOBAL_PYBIND11 = "On"
 ALPAQA_PYTHON_DEBUG_CONFIG = "Debug"
 ALPAQA_WITH_PY_STUBS = "On"
+ALPAQA_WITH_CUTEST = "On"
 EOF
 . ./.venv/bin/activate
 pip install -U pip build
-develop=true
+develop=false
 if $develop; then
     LDFLAGS='-static-libgcc -static-libstdc++' \
     pip install -e ".[test]" -v \
@@ -78,8 +90,8 @@ else
         -C--cross="$pfx/$triple.py-build-cmake.cross.toml" \
         -C--local="$PWD/$config"
     pip install -f staging --force-reinstall --no-deps \
-        "alpaqa==1.0.0a12" "alpaqa-debug==1.0.0a12"
+        "alpaqa==1.0.0a13.dev0" "alpaqa-debug==1.0.0a13.dev0"
     pip install -f staging \
-        "alpaqa[test]==1.0.0a12" "alpaqa-debug==1.0.0a12"
+        "alpaqa[test]==1.0.0a13.dev0" "alpaqa-debug==1.0.0a13.dev0"
 fi
 pytest
