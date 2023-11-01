b5aa40093343d5e9456a7b686c5b7b60be1ea847
Pieter Pas <pieter.pas@outlook.com>
2023-11-01 17:08:56 +0100

    Update LBFGSB driver

--- src/alpaqa/src/driver/lbfgsb-driver.cpp
+++ src/alpaqa/src/driver/lbfgsb-driver.cpp
@@ -5,6 +5,7 @@
 
 #include "alm-driver.hpp"
 #include "cancel.hpp"
+#include "extra-stats.hpp"
 #include "lbfgsb-driver.hpp"
 #include "solver-driver.hpp"
 #include "util.hpp"
@@ -29,16 +30,29 @@ SharedSolverWrapper make_lbfgsb_driver_impl(std::string_view direction,
     if (!direction.empty())
         throw std::invalid_argument(
             "L-BFGS-B solver does not support any directions");
+    using collector_t = AlpaqaSolverStatsCollector<config_t>;
+    std::shared_ptr<collector_t> collector;
     auto inner_solver = make_inner_lbfgsb_solver(opts);
-    auto solver       = make_alm_solver(std::move(inner_solver), opts);
-    unsigned N_exp    = 0;
+    bool extra_stats  = false;
+    set_params(extra_stats, "extra_stats", opts);
+    if (extra_stats) {
+        collector = std::make_shared<collector_t>();
+        inner_solver.set_progress_callback(
+            [collector](const auto &progress_info) {
+                collector->update_iter(progress_info);
+            });
+    }
+    auto solver    = make_alm_solver(std::move(inner_solver), opts);
+    unsigned N_exp = 0;
     set_params(N_exp, "num_exp", opts);
-    return std::make_shared<SolverWrapper>(
-        [solver{std::move(solver)}, N_exp](
-            LoadedProblem &problem, std::ostream &os) mutable -> SolverResults {
-            auto cancel = alpaqa::attach_cancellation(solver);
-            return run_alm_solver(problem, solver, os, N_exp);
-        });
+    auto run = [solver{std::move(solver)},
+                N_exp](LoadedProblem &problem,
+                       std::ostream &os) mutable -> SolverResults {
+        auto cancel = alpaqa::attach_cancellation(solver);
+        return run_alm_solver(problem, solver, os, N_exp);
+    };
+    return std::make_shared<AlpaqaSolverWrapperStats<config_t>>(
+        std::move(run), std::move(collector));
 }
 
 } // namespace
