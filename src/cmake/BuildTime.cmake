0064663d352a7bf1134b4f4445681045a59c03f0
Pieter Pas <pieter.pas@outlook.com>
2023-11-13 11:34:57 +0100

    Extern C linkage for alpaqa_build_time

--- src/cmake/BuildTime.cmake
+++ src/cmake/BuildTime.cmake
@@ -1,3 +1,3 @@
 string(TIMESTAMP ALPAQA_BUILD_TIME UTC)
-configure_file(${CMAKE_CURRENT_LIST_DIR}/alpaqa-build-time.c.in
-    alpaqa-build-time.c @ONLY)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/alpaqa-build-time.cpp.in
+    alpaqa-build-time.cpp @ONLY)
