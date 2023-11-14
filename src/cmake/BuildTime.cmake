00959c03069236c46544f2fab82e6d1a697d40b2
Pieter Pas <pieter.pas@outlook.com>
2023-11-14 17:54:56 +0100

    Add ALPAQA_COMMIT_HASH variable

--- src/cmake/BuildTime.cmake
+++ src/cmake/BuildTime.cmake
@@ -1,3 +1,9 @@
 string(TIMESTAMP ALPAQA_BUILD_TIME UTC)
+execute_process(
+    COMMAND git log -1 --format=%H
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+    OUTPUT_VARIABLE ALPAQA_COMMIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET)
 configure_file(${CMAKE_CURRENT_LIST_DIR}/alpaqa-build-time.cpp.in
     alpaqa-build-time.cpp @ONLY)
