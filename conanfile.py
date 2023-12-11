061e2eb428bee216a66381b6433e7abef30a2e6b
Pieter Pas <pieter.pas@outlook.com>
2023-12-11 19:23:16 +0100

    Version 1.0.0a16.dev0

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ from conan.tools.build import can_run
 
 class AlpaqaRecipe(ConanFile):
     name = "alpaqa"
-    version = "1.0.0-alpha.15"
+    version = "1.0.0-alpha.16"
 
     # Optional metadata
     license = "LGPLv3"
