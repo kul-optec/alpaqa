9c0dde81c0787f95b7ebaff12aa6447dad06ef9e
Pieter Pas <pieter.pas@outlook.com>
2023-12-12 13:40:09 +0100

    Version 1.0.0a16

--- scripts/dev/install-locally.sh
+++ scripts/dev/install-locally.sh
@@ -90,8 +90,8 @@ else
         -C--cross="$pfx/$triple.py-build-cmake.cross.toml" \
         -C--local="$PWD/$config"
     pip install -f staging --force-reinstall --no-deps \
-        "alpaqa==1.0.0a16.dev0" "alpaqa-debug==1.0.0a16.dev0"
+        "alpaqa==1.0.0a16" "alpaqa-debug==1.0.0a16"
     pip install -f staging \
-        "alpaqa[test]==1.0.0a16.dev0" "alpaqa-debug==1.0.0a16.dev0"
+        "alpaqa[test]==1.0.0a16" "alpaqa-debug==1.0.0a16"
 fi
 pytest
