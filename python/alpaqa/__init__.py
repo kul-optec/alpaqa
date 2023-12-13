2d48967d90968837b8a152c8d4318cb102ed7361
Pieter Pas <pieter.pas@outlook.com>
2023-12-13 18:12:22 +0100

    Version 1.0.0a17.dev0

--- python/alpaqa/__init__.py
+++ python/alpaqa/__init__.py
@@ -1,7 +1,7 @@
 """
 Augmented Lagrangian and PANOC solvers for nonconvex numerical optimization.
 """
-__version__ = "1.0.0a16"
+__version__ = "1.0.0a17.dev0"
 
 from .alpaqa import *
 from .alpaqa import __c_version__
