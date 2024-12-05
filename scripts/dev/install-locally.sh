f96e9d44d9ef3964f83797f31fe3508246593bb5
Pieter Pas <pieter.p.dev@outlook.com>
2024-12-05 11:14:52 +0100

    Improve install-locally.sh

--- scripts/dev/install-locally.sh
+++ scripts/dev/install-locally.sh
@@ -3,6 +3,16 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"/../..
 
 set -ex
 
+build_python=1
+build_cpp=1
+
+case $1 in
+    "") ;;
+    py*) build_cpp=0 ;;
+    cpp*) build_python=0 ;;
+    *) echo "Invalid argument, use python or cpp"; exit 1 ;;
+esac
+
 # Activate virtual environment
 [ -d ".venv" ] || python3 -m venv .venv
 . ./.venv/bin/activate
@@ -61,44 +71,8 @@ CFLAGS+= -march=native -fdiagnostics-color
 CXXFLAGS+= -march=native -fdiagnostics-color
 LDFLAGS+= -static-libgcc -static-libstdc++ -flto=auto
 EOF
-
-# Build and install alpaqa dependencies
-export CTEST_OUTPUT_ON_FAILURE=1
-for cfg in Debug RelWithDebInfo; do
-    conan install . --build=missing \
-        -pr:h "$profile" \
-        -o \&:shared=True \
-        -o \&:with_ipopt=True -o \&:with_external_casadi=True \
-        -o \&:with_qpalm=True -o \&:with_cutest=True \
-        -o \&:with_examples=True \
-        -o "openblas/*:target=HASWELL" \
-        -s "casadi/*:build_type=Release" \
-        -s build_type=$cfg
-done
-
-# Configure
-cmake --preset conan-default -B build \
-    -D CMAKE_EXPORT_COMPILE_COMMANDS=On \
-    -D CMAKE_INSTALL_PREFIX="$PWD/staging" \
-    -D CMAKE_C_COMPILER_LAUNCHER="ccache" \
-    -D CMAKE_CXX_COMPILER_LAUNCHER="ccache"
-# Build
-for cfg in Debug RelWithDebInfo; do
-    cmake --build build -j --config $cfg
-    cmake --install build --config $cfg
-    cmake --install build --config $cfg --component debug
-done
-# Package
-pushd build
-cpack -G 'TGZ;DEB' -C "RelWithDebInfo;Debug"
-popd
-
-# Build Python package
-for cfg in Debug Release; do
-    conan install . --build=missing -pr:h "$python_profile" -s build_type=$cfg
-done
-config="$triple.py-build-cmake.config.pbc"
-cat <<- EOF > "$config"
+pbc_config="$PWD/$triple.py-build-cmake.config.pbc"
+cat <<- EOF > "$pbc_config"
 os=linux
 toolchain_file=!  # Set by the Conan preset
 cmake.options.CMAKE_C_COMPILER_LAUNCHER="ccache"
@@ -106,29 +80,69 @@ cmake.options.CMAKE_CXX_COMPILER_LAUNCHER="ccache"
 cmake.options.ALPAQA_WITH_PY_STUBS=true
 cmake.options.USE_GLOBAL_PYBIND11=true
 EOF
-cross_cfg="$pfx.python$python_majmin.py-build-cmake.cross.toml"
-develop=false
-if $develop; then
-    pip install -e ".[test]" -v \
-        -C local="$PWD/scripts/ci/py-build-cmake.toml" \
-        -C cross="$cross_cfg" \
-        -C cross="$PWD/$config"
-else
-    tag=\"$(date -u +"%Y_%m_%dT%H.%M.%SZ")\"
-    python -m build -w "." -o staging \
-        -C local="$PWD/scripts/ci/py-build-cmake.toml" \
-        -C cross="$cross_cfg" \
-        -C cross="$PWD/$config" \
-        -C override=wheel.build_tag="$tag"
-    python -m build -w "python/alpaqa-debug" -o staging \
-        -C local="$PWD/scripts/ci/py-build-cmake.toml" \
-        -C component="$PWD/scripts/ci/py-build-cmake.component.toml" \
-        -C cross="$cross_cfg" \
-        -C cross="$PWD/$config" \
-        -C override=wheel.build_tag="$tag"
-    pip install -f staging --force-reinstall --no-deps \
-        "alpaqa==1.0.0a20" "alpaqa-debug==1.0.0a20"
-    pip install -f staging \
-        "alpaqa[test]==1.0.0a20" "alpaqa-debug==1.0.0a20"
+
+if [ $build_cpp -eq 1 ]; then
+    # Build and install alpaqa dependencies
+    export CTEST_OUTPUT_ON_FAILURE=1
+    for cfg in Debug RelWithDebInfo; do
+        conan install . --build=missing \
+            -pr:h "$profile" \
+            -o \&:shared=True \
+            -o \&:with_ipopt=True -o \&:with_external_casadi=True \
+            -o \&:with_qpalm=True -o \&:with_cutest=True \
+            -o \&:with_examples=True \
+            -o "openblas/*:target=HASWELL" \
+            -s "casadi/*:build_type=Release" \
+            -s build_type=$cfg
+    done
+
+    # Configure
+    cmake --preset conan-default -B build \
+        -D CMAKE_EXPORT_COMPILE_COMMANDS=On \
+        -D CMAKE_INSTALL_PREFIX="$PWD/staging" \
+        -D CMAKE_C_COMPILER_LAUNCHER="ccache" \
+        -D CMAKE_CXX_COMPILER_LAUNCHER="ccache"
+    # Build
+    for cfg in Debug RelWithDebInfo; do
+        cmake --build build -j --config $cfg
+        cmake --install build --config $cfg
+        cmake --install build --config $cfg --component debug
+    done
+    # Package
+    pushd build
+    cpack -G 'TGZ;DEB' -C "RelWithDebInfo;Debug"
+    popd
 fi
-pytest
+
+# Build Python package
+if [ $build_python -eq 1 ]; then
+    for cfg in Debug Release; do
+        conan install . --build=missing -pr:h "$python_profile" -s build_type=$cfg
+    done
+    cross_cfg="$pfx.python$python_majmin.py-build-cmake.cross.toml"
+    develop=false
+    if $develop; then
+        pip install -e ".[test]" -v \
+            -C local="$PWD/scripts/ci/py-build-cmake.toml" \
+            -C cross="$cross_cfg" \
+            -C cross="$pbc_config"
+    else
+        tag=\"$(date -u +"%Y_%m_%dT%H.%M.%SZ")\"
+        python -m build -w "." -o staging \
+            -C local="$PWD/scripts/ci/py-build-cmake.toml" \
+            -C cross="$cross_cfg" \
+            -C cross="$pbc_config" \
+            -C override=wheel.build_tag="$tag"
+        python -m build -w "python/alpaqa-debug" -o staging \
+            -C local="$PWD/scripts/ci/py-build-cmake.toml" \
+            -C component="$PWD/scripts/ci/py-build-cmake.component.toml" \
+            -C cross="$cross_cfg" \
+            -C cross="$pbc_config" \
+            -C override=wheel.build_tag="$tag"
+        pip install -f staging --force-reinstall --no-deps \
+            "alpaqa==1.0.0a20.dev0" "alpaqa-debug==1.0.0a20.dev0"
+        pip install -f staging \
+            "alpaqa[test]==1.0.0a20.dev0" "alpaqa-debug==1.0.0a20.dev0"
+    fi
+    pytest
+fi
