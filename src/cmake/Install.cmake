7d01df1254bc3ce4a8c012096b8d6ad1b9cbd8d6
Pieter Pas <pieter.p.dev@outlook.com>
2025-02-15 11:45:21 +0100

    Install problem-loader library

--- src/cmake/Install.cmake
+++ src/cmake/Install.cmake
@@ -76,7 +76,7 @@ endmacro()
 set(ALPAQA_INSTALLED_TARGETS_MSG "\nSummary of alpaqa components and targets to install:\n\n")
 
 # Install the alpaqa core libraries
-install(TARGETS warnings alpaqa
+install(TARGETS warnings alpaqa problem-loader
     EXPORT alpaqaCoreTargets
     RUNTIME DESTINATION "${ALPAQA_INSTALL_BINDIR}"
         COMPONENT lib
@@ -89,7 +89,7 @@ alpaqa_install_config(Core dev)
 alpaqa_install_headers("${PROJECT_BINARY_DIR}/include/" dev)
 alpaqa_install_headers("${PROJECT_SOURCE_DIR}/src/alpaqa/include/" dev)
 alpaqa_install_headers("${CMAKE_CURRENT_BINARY_DIR}/export/" dev)
-string(APPEND ALPAQA_INSTALLED_TARGETS_MSG " * Core:   alpaqa\n")
+string(APPEND ALPAQA_INSTALLED_TARGETS_MSG " * Core:   alpaqa, problem-loader\n")
 
 # Install the CasADi interface
 alpaqa_add_if_target_exists(ALPAQA_COMPONENT_CASADI_TARGETS "casadi-loader")
