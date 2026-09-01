8d8b0b57e444dbcf4b61f5d05dca3a0cff6913b7
Pieter Pas <pieter.p.dev@outlook.com>
2026-09-01 14:06:16 +0200

    Upgrade QPALM

--- conanfile.py
+++ conanfile.py
@@ -97,7 +97,7 @@ class AlpaqaRecipe(ConanFile):
         if self.options.with_ipopt:
             self.requires("ipopt/3.14.16", transitive_headers=True)
         if self.options.with_qpalm:
-            self.requires("qpalm/1.2.6", transitive_headers=True)
+            self.requires("qpalm/[>=1.2.6 <1.4.0]", transitive_headers=True)
         if self.options.with_python or self.options.with_python_problem_loader:
             self.requires("pybind11/2.13.6")
             if self.options.with_conan_python:
